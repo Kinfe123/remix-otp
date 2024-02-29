3ada62b4d5a0dc43681c486e6108ae8f3aabc7d5
Kinfe123 <kinfishtech@gmail.com>
2024-02-29 17:42:38 +0300

    libsql to better-sqlite driver mig

--- app/db/config.server.ts
+++ app/db/config.server.ts
@@ -3,7 +3,6 @@ import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 
 const sqlite = new Database("sqlite.db");
-
+// driver and url = better-sqlite and ./sqlite.db on the root dir
 export const db = drizzle(sqlite);
-
 migrate(db, { migrationsFolder: "./app/db/migrations" });
