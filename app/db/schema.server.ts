09b1d60843df8d0ad56d0dad9e5cd046a1b25e42
Kinfe123 <kinfishtech@gmail.com>
2024-02-29 17:07:08 +0300

    added migrations

--- app/db/schema.server.ts
+++ app/db/schema.server.ts
@@ -3,6 +3,6 @@ import { sqliteTable, text, integer  } from "drizzle-orm/sqlite-core";
 export const opts = sqliteTable("otps", {
   id: integer("id").primaryKey(),
   name:text('name').notNull(),
-  opt: text("optcode").notNull(),
+  opt: integer("optcode").notNull(),
   email: text("email").notNull(),
 });
--- app/server/accoun.server.ts
+++ app/server/accoun.server.ts
@@ -1,7 +1,7 @@
 import { db } from "~/db/config.server"
 import { opts } from "~/db/schema.server"
-export const createAccount = async ({name , email , pass}: {name: string , email: string , pass:string}) => {
-    const req = await db.insert(opts).values({email: email , name: name , })
+export const createAccount = async ({name , email , opt}: {name: string , email: string , opt:number}) => {
+    const req = await db.insert(opts).values({email: email , name: name , opt:otp })
     console.log(name , email , pass)
 
 }
